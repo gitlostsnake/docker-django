6012fa5db47f2fa1acd82bef859318ba6a80d517
Michael Hughes <michaeldanhughes92@gmail.com>
2025-03-19 07:03:16 +0000

    Working on urls and templates

--- project/settings.py
+++ project/settings.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+def base_path(path):
+    return os.path.join(BASE_DIR, path)
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +60,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [base_path('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
