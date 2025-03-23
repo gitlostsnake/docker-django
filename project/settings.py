f43a3eef0fc5575eed8fe04ab9b662ea602bddd1
Michael Hughes <michaeldanhughes92@gmail.com>
2025-03-23 10:50:04 +0000

    Static set up for local dev

--- project/settings.py
+++ project/settings.py
@@ -128,6 +128,8 @@ LOGOUT_REDIRECT_URL = "/admin"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
